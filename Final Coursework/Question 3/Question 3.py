647b7e8dbba578927259e88690c5e0ad08925830
callumastorey <Storeyc2@uni.coventry.ac.uk>
2016-12-01 22:07:22 +0000

    Update Question 3.py

--- Final Coursework/Question 3/Question 3.py
+++ Final Coursework/Question 3/Question 3.py
@@ -1,18 +1,39 @@
-__author__ = 'Callum'
-#take an integer
-theInt = int(input(("Type an integer: ")))
-#square root the integer
-#theInt = math.sqrt(theInt)
-theInt = theInt**.5
+###QUESTION 3###
 
-#convert float back to integer
-theInt = int(theInt)
+usersInt = int(input(("Type an integer: ")))
 
-#times te number by itself
-theInt = theInt *theInt
+def highestPerfSqr(usersInt):
+    """Function to find the highest perfect square root from a given integer"""
+    result = usersInt**.5 # Finding the square root of the given number
+    result = int(result) # Turning the float back to an integer rounds down to the nearest whole number
+    result = result * result # Multiplying the result by itself to find the new square number
+    return result # Returning the result
 
-#print result
-print(theInt)
+print(highestPerfSqr(usersInt))
 
 
-#big o notaion o to the 1
+"""Pseudocode"""
+
+"""
+INPUT = INPUT(int)
+
+FUNCTION highestPerfSqr(I)
+    RESULT = square root(I)
+    RESULT = round down(I)
+    RESULT = RESULT * RESULT
+    RETURN RESULT
+
+PRINT(highestPerfSqr(INPUT))
+"""
+
+
+"""Description"""
+"""This is a function to find the highest perfect square number from a given integer. The theory is really
+quite simple. All we really need to do is square root the given integer, we will most likely be returned
+with an un-even float. With python you can easily round down to the nearest positive integer by changing
+the variable from a float to an integer. Once it is an integer you can easily return the square root value
+of that integer."""
+
+
+"""Big O Notation"""
+"""The big o notation for this question is 0(1)"""
