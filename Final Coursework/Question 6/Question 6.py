7702c28142a48bda647362301fd46bbb0bd07ede
callumastorey <Storeyc2@uni.coventry.ac.uk>
2016-12-02 01:43:15 +0000

    Update Question 6.py

--- Final Coursework/Question 6/Question 6.py
+++ Final Coursework/Question 6/Question 6.py
@@ -1,24 +1,33 @@
-__author__ = 'Callum'
-"""Write the pseudocode and code for a function that reverses
-the words in a sentence. Input: "This is awesome"
-Output: "awesome is This". Give the Big O notation."""
+###QUESTION 6###
 
-#split string into words using split.word(might have to change to something else
+givenStr = "This is awesome"
 
-reverseString = "This is awesome"
-#print(reverseString)
-reverseString = reverseString.split()
-#print(reverseString)
+def sentReverse(string):
+    """Function to reverse the order of a given string"""
+    string = string.split() # Splits the string into a list
+    string.reverse() # Reverses the order of the list
+    string = " ".join(string) # Joins the elements of the list together with a space inbwtween the words, to form a str
+    return string # Returns the reversed string
 
-#reverse list
-reverseString.reverse()
-#print(reverseString)
-reverseString = " ".join(reverseString)
+print(sentReverse(givenStr))
 
+"""Pseudocode"""
 
-#print list
-print(reverseString)
+"""
+FUNCTION sentReverse(INPUT):
+    INPUT = INPUT.SPLIT()
+    INPUT.REVERSE()
+    INPUT.JOIN(INPUT)
+    RETURN
 
+PRINT(sentReverse(INPUT))
+"""
 
-#big o notation
+"""Description"""
+"""This is another simple function that takes a given string and reverses the order of the words and returns.
+The program first splits the string into a list, each word in its own element. The list is then reversed
+using the list.reverse function, we then join the list back together to form a string then return."""
+
+"""Big O Notation"""
+"""The big o notation for this question is 0(1)"""
 
