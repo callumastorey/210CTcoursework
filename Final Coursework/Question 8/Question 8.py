01c971fee1d34995ba8f9b4e3ad215065d7a1573
callumastorey <Storeyc2@uni.coventry.ac.uk>
2016-12-02 02:59:49 +0000

    Update Question 8.py

--- Final Coursework/Question 8/Question 8.py
+++ Final Coursework/Question 8/Question 8.py
@@ -1,76 +1,39 @@
-__author__ = 'Callum'
-"""Write a recursive function (pseudocode and code) that removes all vowels from a given string s.
-Input: "beautiful" Output: "btfl"."""
-
-#create dict with vowels
+###QUESTION 8###
 
 vowels = ['a','e','i','o','u']
-#print(vowels)
-#take word, convert to list
-chosenWord = str(input("Type in a word: "))
-#print(chosenWord)
-#print(chosenWord)
+givenWord = str(input("Type in a word: "))
+
+def removeVowels(counter,givenWord, vowels):
+    """Function to remove vowels from a given word"""
+    if counter > 4: # If counter is more than 4, it means that there are no vowels remaining in chosenWord
+        return givenWord
+    for i in givenWord: # For each character in chosenWord
+        if i == vowels[counter]: # If character matches the currently selected vowel
+             givenWord = givenWord.replace(vowels[counter], "") # Remove the currently selected vowel from chosenWord
+    return removeVowels(counter+1, givenWord, vowels) # Return to beginning of function, +1 counter for next vowel
+print(removeVowels(0,givenWord,vowels))
+
+"""Pseudocode"""
 
 """
-vowels      ----> list with vowels
-chosenWord  ----> List of letters in word
+INPUT = vowels
+INPUT = STRING
 
-
-def removeVowels():
-    if vowel is in chosenWord:
-        chosen
+FUNCTION removeVowels(counter, INPUT, vowels):
+    IF counter > 4:
+        RETURN INPUT
+    FOR I IN INPUT:
+        IF I = vowels[counter]:
+            INPUT = INPUT.REMOVE(vowel)
+    RETURN removeVowels(counter-1, INPUT, vowels)
+PRINT(removeVowels(0, INPUT, vowels)
 
 """
 
-"""
-newList = chosenWord-vowels
-print(newList)
+"""Description"""
+"""This recursive program removes vowels from a given string. A vowel list is given to the program and each
+time the function runs a different vowel is compared to each character in the given word. A counter is used
+ to track when all the vowels have been searched for."""
 
-#ITERATIVE FUNTION
-#numberOfLetters = len(chosenWord)
-#for i in range(0,numberOfLetters):
-
-position <- 0
-found <- False
-while position < len(List) and not found:
-    if List[position] = item:
-        found <- True
-    position <- position + 1"""
-
-
-
-
-def removeVowels(I,chosenWord, vowels):
-    if I>4:
-        return chosenWord
-    for x in chosenWord:
-        #run linear search to find item in list
-        if x == vowels[I]:
-             chosenWord = chosenWord.replace(vowels[I], "")
-
-    return removeVowels(I+1, chosenWord, vowels)
-
-
-
-print(removeVowels(0,chosenWord,vowels))
-
-
-
-""""
-def LinearSearch(chosenWord, vowels):
-    found = false
-    position = 0
-    while position < len(vowels and not found):
-        if vowels[position == chosenWord]"""
-
-
-
-
-#check word for vowels MAIN FUNCTION RECURSIVE
-
-#def removeVowels():
- #   if vowels isnt in
-
-
-#remove vowels from word
-#print word
+"""Big O Notation"""
+"""The big o notation for this question is 0(n)"""
